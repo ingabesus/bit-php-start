3dc2b844509d3e2dcff34f2423860ce4f05737c5
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-12-06 18:29:19 +0200

    Funkcijos

---
+++ funkcijos.php
@@ -0,0 +1,13 @@
+<?php
+
+function prideti_penkiolika($kintamasis) {
+    echo 'Testinis tekstas <br />';
+
+    return $kintamasis + 15;
+}
+
+$x = prideti_penkiolika(25);
+echo $x . '<br />';
+
+
+?>
