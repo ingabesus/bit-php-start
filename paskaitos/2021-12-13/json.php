f6d1498aa245e44b1240c08eacaef4d0c80e102e
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-12-13 21:11:47 +0200

    Custom functions implementation

--- paskaitos/2021-12-13/json.php
+++ paskaitos/2021-12-13/json.php
@@ -2,6 +2,18 @@
 
 $target = './db.json';
 
+function open_db_file($link) {
+
+    $failas = fopen($link, 'r');
+    $json = fread($failas, filesize($link));
+    fclose($failas);
+
+    $json = json_decode($json, true);
+
+    return $json;
+
+}
+
 //Masyvo sugeneravimas
 
 if(isset($_GET['action']) AND $_GET['action'] == 'sukurti_masyva') {
@@ -36,9 +48,9 @@ if(isset($_GET['action']) AND $_GET['action'] == 'sukurti_masyva') {
 
 if(isset($_GET['action']) AND $_GET['action'] == 'atvaizduoti_masyva') {
 
-    $failas = fopen($target, 'r');
-    $json = fread($failas, filesize($target));
-    fclose($failas);
+    // $failas = fopen($target, 'r');
+    // $json = fread($failas, filesize($target));
+    // fclose($failas);
 
     //file_get_contents() failu atidarymo variantas
     //$json = file_get_contents($target);
@@ -49,11 +61,40 @@ if(isset($_GET['action']) AND $_GET['action'] == 'atvaizduoti_masyva') {
     //echo $std->indeksas->masyvas[0];
 
     //I Asociatyvaus Tipo masyva dekodintas JSON stringas
-    $json = json_decode($json, true);
+    //$json = json_decode($json, true);
 
+
+    $json = open_db_file($target);
 
     echo '<pre>';
     print_r($json);
+
+}
+
+if(isset($_GET['action']) AND $_GET['action'] == 'filtruoti_masyva') {
+
+        // $json = file_get_contents($target);
+        // $json = json_decode($json);
+
+        $json = open_db_file($target);
+
+        foreach($json as $key => $val) {
+
+            foreach($val as $kchild => $kval) {
+
+                if($kval > 6500) {
+                    unset($json[$key][$kchild]);
+                }
+
+            }
+
+        }
+
+        $json = json_encode($json);
+
+        if( file_put_contents($target, $json) ) {
+            echo 'Sekmingai isfiltravote JSON stringa';
+        }
 
 }
 
@@ -61,3 +102,4 @@ if(isset($_GET['action']) AND $_GET['action'] == 'atvaizduoti_masyva') {
 
 <a href="?action=sukurti_masyva">Sukurti masyvą</a>
 <a href="?action=atvaizduoti_masyva">Atvaizduoti masyva</a>
+<a href="?action=filtruoti_masyva">Filtruoti masyva</a>
