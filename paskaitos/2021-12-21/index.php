333649d84a7c366660836c8f09b2e0c39b1c8b38
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-12-21 18:30:28 +0200

    Create index.php

---
+++ paskaitos/2021-12-21/index.php
@@ -0,0 +1,35 @@
+<?php
+//Klasės ir Objektai
+
+//Klasė
+class Test {
+
+    //Trys matomumo variantai 
+    //public
+    //private
+    //protected 
+
+    //Savybes | Properties
+    private $name;
+
+    //Metodas | Method
+    public function setName($name) {
+        //$this - yra kreipimasis į funkciją kurioje yra metodas
+        $this->name = $name;
+    }
+
+    private function getName() {
+        //$this->setName();
+        return $this->name;
+    }
+
+}
+
+//Objektas
+$test = new Test();
+
+$test->setName('Vilius');
+
+//echo $test->name;
+
+echo $test->getName();
