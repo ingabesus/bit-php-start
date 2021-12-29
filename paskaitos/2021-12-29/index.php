dc9782dd8242f677aa0ed2d3518f4fc60e284123
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-12-29 18:52:47 +0200

    Create index.php

---
+++ paskaitos/2021-12-29/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//$objektas = new stdClass;
+$objektas = (object);
+
+// $masyvas = [];
+// $masyvas = array('1234', 1234);
+
+$objektas->test = 1234;
+$objektas->test1 = false;
+$objektas->test2 = 'Test';
+
+//echo $objektas->test;
+
+print_r($objektas);
